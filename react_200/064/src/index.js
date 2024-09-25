39d823b4a8060a2b10f00aaf691daee00286378c
unknown <hoon7812@jobko.kr>
2024-09-25 10:33:15 +0900

    새로 작업

---
+++ react_200/064/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// 앱이 오프라인에서 작동하고 더 빠르게 로드되도록 하려면
+// unregister()를 아래에서 register()로 변경할 수 있습니다. 여기에는 몇 가지 함정이 있습니다.
+// 서비스 워커에 대해 자세히 알아보세요: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
